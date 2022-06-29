e0eccefb0f3dbadf1f13ea7f2d85edf3ccd56387
alainjr10 <njokomalain@gmail.com>
2022-06-29 21:20:55 +0100

    fixed failed to load asset image problem

--- lib/services/classify_image.dart
+++ lib/services/classify_image.dart
@@ -1,16 +1,23 @@
 import 'dart:io';
+import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite/tflite.dart';
+import 'package:image/image.dart' as img;
+import 'package:progress_dialog_null_safe/progress_dialog_null_safe.dart';
 
 class Classifier {
   late File imageFile;
   late List outputs;
+  ProgressDialog? pr;
 
   Future<List?> getDisease({required ImageSource imageSource}) async {
     var image = await ImagePicker().pickImage(source: imageSource);
     if (image == null) return null;
     imageFile = File(image.path);
+
     await loadModel();
     var result = await classifyImage(imageFile);
     Tflite.close();
@@ -20,8 +27,11 @@ class Classifier {
   Future loadModel() async {
     /// Load the Tensorflow Lite model.
     await Tflite.loadModel(
-        model: 'assets/models/model_unquant.tflite',
-        labels: 'assets/models/labels.txt');
+            model: 'assets/models/model_unquant.tflite',
+            labels: 'assets/models/labels.txt')
+        .then((value) {
+      debugPrint("Model loaded successfully");
+    });
   }
 
   Future<List?> classifyImage(File image) async {
@@ -38,5 +48,52 @@ class Classifier {
     );
     outputs = output!;
     return output;
+  }
+
+////
+  ///
+  ///
+  /// THE TRANSFERRED SECTION
+  ///
+  ///
+  ///
+  ///
+  ///
+  ///
+  Future loadModelfrfr() async {
+    debugPrint("DEBUG: Inside Load Model Function");
+    //Tflite.close();
+    try {
+      String? res;
+      res = await Tflite.loadModel(
+        model: "assets/alt/model.tflite",
+        labels: "assets/alt/labels.txt",
+      );
+      debugPrint(res);
+    }
+    //  on PlatformException {
+    //   print('Failed to load model.');
+    //   showErrorProcessing(context);
+    // }
+    catch (e) {
+      debugPrint("Error while loading model $e");
+      //showErrorProcessing(context);
+    }
+  }
+
+  Uint8List imageToByteListFloat32(
+      img.Image image, int inputSize, double mean, double std) {
+    var convertedBytes = Float32List(1 * inputSize * inputSize * 3);
+    var buffer = Float32List.view(convertedBytes.buffer);
+    int pixelIndex = 0;
+    for (var i = 0; i < inputSize; i++) {
+      for (var j = 0; j < inputSize; j++) {
+        var pixel = image.getPixel(j, i);
+        buffer[pixelIndex++] = (img.getRed(pixel) - mean) / std;
+        buffer[pixelIndex++] = (img.getGreen(pixel) - mean) / std;
+        buffer[pixelIndex++] = (img.getBlue(pixel) - mean) / std;
+      }
+    }
+    return convertedBytes.buffer.asUint8List();
   }
 }
--- lib/views/storage_centers_screen.dart
+++ lib/views/storage_centers_screen.dart
@@ -14,34 +14,34 @@ class StorageCenters extends StatefulWidget {
 }
 
 class _StorageCentersState extends State<StorageCenters> {
-  late GoogleMapController mapController;
+  // late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(25.521563, 22.677433);
-  MapType _mapType = MapType.normal;
-  final Location _location = Location();
+  // final LatLng _center = const LatLng(25.521563, 22.677433);
+  // MapType _mapType = MapType.normal;
+  // final Location _location = Location();
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-    _location.getLocation().then((value) {
-      debugPrint("Latitude: ${value.latitude}, longitude: ${value.longitude}");
-      mapController.animateCamera(CameraUpdate.newCameraPosition(
-        CameraPosition(
-            target: LatLng(value.latitude!, value.longitude!), zoom: 15),
-      ));
-    });
-  }
+  // void _onMapCreated(GoogleMapController controller) {
+  //   mapController = controller;
+  //   _location.getLocation().then((value) {
+  //     debugPrint("Latitude: ${value.latitude}, longitude: ${value.longitude}");
+  //     mapController.animateCamera(CameraUpdate.newCameraPosition(
+  //       CameraPosition(
+  //           target: LatLng(value.latitude!, value.longitude!), zoom: 15),
+  //     ));
+  //   });
+  // }
 
-  void _toggleMapType() {
-    if (_mapType == MapType.normal) {
-      setState(() {
-        _mapType = MapType.satellite;
-      });
-    } else if (_mapType == MapType.satellite) {
-      setState(() {
-        _mapType = MapType.normal;
-      });
-    }
-  }
+  // void _toggleMapType() {
+  //   if (_mapType == MapType.normal) {
+  //     setState(() {
+  //       _mapType = MapType.satellite;
+  //     });
+  //   } else if (_mapType == MapType.satellite) {
+  //     setState(() {
+  //       _mapType = MapType.normal;
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -52,39 +52,39 @@ class _StorageCentersState extends State<StorageCenters> {
         backgroundColor: kPrimaryColor,
         centerTitle: true,
       ),
-      body: Stack(
-        children: [
-          GoogleMap(
-            myLocationEnabled: true,
-            myLocationButtonEnabled: true,
-            mapType: _mapType,
-            onMapCreated: _onMapCreated,
-            markers: StorageCentersMarkerLocations().getmarkers(context, size),
-            initialCameraPosition: CameraPosition(
-              target: _center,
-              zoom: 9.0,
-            ),
-          ),
-          Positioned(
-            right: 12.0,
-            top: 80.0,
-            child: CircleAvatar(
-              backgroundColor: kPrimaryColor,
-              radius: 21.0,
-              child: IconButton(
-                onPressed: () {
-                  _toggleMapType();
-                },
-                icon: const Icon(
-                  Icons.layers_outlined,
-                  size: 27.0,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+      // body: Stack(
+      //   children: [
+      //     GoogleMap(
+      //       myLocationEnabled: true,
+      //       myLocationButtonEnabled: true,
+      //       mapType: _mapType,
+      //       onMapCreated: _onMapCreated,
+      //       markers: StorageCentersMarkerLocations().getmarkers(context, size),
+      //       initialCameraPosition: CameraPosition(
+      //         target: _center,
+      //         zoom: 9.0,
+      //       ),
+      //     ),
+      //     Positioned(
+      //       right: 12.0,
+      //       top: 80.0,
+      //       child: CircleAvatar(
+      //         backgroundColor: kPrimaryColor,
+      //         radius: 21.0,
+      //         child: IconButton(
+      //           onPressed: () {
+      //             _toggleMapType();
+      //           },
+      //           icon: const Icon(
+      //             Icons.layers_outlined,
+      //             size: 27.0,
+      //             color: Colors.white,
+      //           ),
+      //         ),
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
